d77c336ec33c90083264ba01de7134fde66cbbd8
Obydux <obydux@proton.me>
2024-01-25 11:50:56 +0000

    init

---
+++ README.md
@@ -0,0 +1,26 @@
+[discord]: https://discord.gg/9MwSVT7Xt7
+[downloads]: https://github.com/Obydux/Boxfish/releases
+[optguide]: https://docs.pufferfish.host/optimization/pufferfish-server-optimization-guide/
+
+# Boxfish
+A slightly more optimized Pufferfish fork.
+
+[Downloads][downloads] - [Discord][discord] - [Optimization Guide][optguide]
+
+## Features
+
+- TBA
+
+## Downloads
+You can download the latest JAR file [here][downloads].
+
+## Building
+
+```bash
+./gradlew applyPatches
+./gradlew createReobfPaperclipJar
+```
+
+## License
+Patches are licensed under GPL-3.0.
+All other files are licensed under MIT.
---
+++ build.gradle.kts
@@ -0,0 +1,68 @@
+import io.papermc.paperweight.util.constants.PAPERCLIP_CONFIG
+
+plugins {
+    java
+    id("com.github.johnrengelman.shadow") version "8.1.1" apply false
+    id("io.papermc.paperweight.patcher") version "1.5.11"
+}
+
+repositories {
+    mavenCentral()
+    maven("https://papermc.io/repo/repository/maven-public/") {
+        content { onlyForConfigurations(PAPERCLIP_CONFIG) }
+    }
+}
+
+dependencies {
+    remapper("net.fabricmc:tiny-remapper:0.8.10:fat")
+    decompiler("net.minecraftforge:forgeflower:2.0.627.2")
+    paperclip("io.papermc:paperclip:3.0.3")
+}
+
+subprojects {
+    apply(plugin = "java")
+
+    java {
+        toolchain { languageVersion.set(JavaLanguageVersion.of(17)) }
+    }
+
+    tasks.withType<JavaCompile>().configureEach {
+        options.encoding = "UTF-8"
+        options.release.set(17)
+    }
+
+    repositories {
+        mavenLocal()
+        mavenCentral()
+        maven("https://oss.sonatype.org/content/groups/public/")
+        maven("https://papermc.io/repo/repository/maven-public/")
+        maven("https://ci.emc.gs/nexus/content/groups/aikar/")
+        maven("https://repo.aikar.co/content/groups/aikar")
+        maven("https://repo.md-5.net/content/repositories/releases/")
+        maven("https://hub.spigotmc.org/nexus/content/groups/public/")
+        maven("https://jitpack.io")
+    }
+}
+
+paperweight {
+    serverProject.set(project(":boxfish-server"))
+
+    remapRepo.set("https://maven.fabricmc.net/")
+    decompileRepo.set("https://files.minecraftforge.net/maven/")
+
+    useStandardUpstream("pufferfish") {
+        url.set(github("pufferfish-gg", "Pufferfish"))
+        ref.set(providers.gradleProperty("pufferfishRef"))
+
+        withStandardPatcher {
+            apiSourceDirPath.set("pufferfish-api")
+            serverSourceDirPath.set("pufferfish-server")
+
+            apiPatchDir.set(layout.projectDirectory.dir("patches/api"))
+            serverPatchDir.set(layout.projectDirectory.dir("patches/server"))
+
+            apiOutputDir.set(layout.projectDirectory.dir("boxfish-api"))
+            serverOutputDir.set(layout.projectDirectory.dir("boxfish-server"))
+        }
+    }
+}
